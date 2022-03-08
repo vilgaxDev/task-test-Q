b9247377e1998ac79bac7f89608c3ca166d9f82f
vilgaxDev <ken.vilgax@gmail.com>
2022-03-08 12:01:06 +0300

    swagger UI

--- backend/src/main/java/com/vilgaxdevtask/test/config/SwaggerConfig.java
+++ backend/src/main/java/com/vilgaxdevtask/test/config/SwaggerConfig.java
@@ -1,2 +1,17 @@
 package com.vilgaxdevtask.test.config;public class SwaggerConfig {
 }
+
+/*public class SwaggerConfig {
+
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }*/
+
+
+    /*public ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title( App API (Kenbrian) ").license("").licenseUrl("").termsOfServiceUrl("")
+                .version("1.0").build();
+    }*/
+
+
--- frontend/src/actions/types.js
+++ frontend/src/actions/types.js
@@ -1,6 +1,6 @@
 export const GET_ERRORS = "GET_ERRORS";
-export const GET_PROJECTS = "GET_PROJECTS";
-export const GET_PROJECT = "GET_PROJECT";
+export const GET_PROJECTS = "GET_PROJECTS"; 
+export const GET_PROJECT = "GET_PROJECT"
 export const DELETE_PROJECT = "DELETE_PROJECT";
 
 //Types for backlog actions
